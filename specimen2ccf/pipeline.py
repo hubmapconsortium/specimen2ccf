969fd8032a31a4e04f750c9c8560fcd62a3e51a2
Josef Hardi <josef.hardi@gmail.com>
2022-02-17 11:49:01 -0800

    Allow both input local or local files

--- specimen2ccf/pipeline.py
+++ specimen2ccf/pipeline.py
@@ -1,5 +1,10 @@
+import json
 import requests
+
+from urllib.parse import urlparse
+from os.path import exists
 from requests_file import FileAdapter
+
 from specimen2ccf.ontology import SCOntology
 
 
@@ -10,9 +15,16 @@ def run(args):
     session.mount('file://', FileAdapter())
 
     o = SCOntology.new(args.ontology_iri)
-    for url in args.input_url:
-        response = session.get(url)
-        data = response.json()
+    for url in args.input_file:
+        data = json.load(open(url)) if is_local(url) \
+            else session.get(url).json()
         o = o.mutate(data)
 
     o.serialize(args.output)
+
+
+def is_local(url):
+    url_parsed = urlparse(url)
+    if url_parsed.scheme in ('file', ''):
+        return exists(url_parsed.path)
+    return False
