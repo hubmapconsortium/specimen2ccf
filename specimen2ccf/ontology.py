21e0d356437a241ada136c0aba8cefad4cead95f
Josef Hardi <josef.hardi@gmail.com>
2022-03-15 16:30:19 -0700

    Code refactoring

--- specimen2ccf/ontology.py
+++ specimen2ccf/ontology.py
@@ -41,14 +41,22 @@ class SCOntology:
     def mutate(self, data):
         """
         """
-        for obj in data['@graph']:
-            object_type = obj['@type']
-            if object_type == "Donor":
-                self._add_donor(obj)
-
+        data_array = data
+        if isinstance(data, dict):
+            data_array = data['@graph']
+        for obj in data_array:
+            publisher = self._get_publisher(obj)
+            self._add_specimen_data(obj, publisher)
         return SCOntology(self.graph)
 
-    def _add_donor(self, obj):
+    def _add_specimen_data(self, obj, publisher):
+        object_type = obj['@type']
+        if object_type == "Donor":
+            self._add_donor(obj, publisher)
+        else:
+            raise ValueError("Unknown object_type <" + object_type + ">")
+
+    def _add_donor(self, obj, publisher):
         donor_iri = self._uri(obj['@id'])
         self._add_donor_to_graph(
             donor_iri,
@@ -60,12 +68,14 @@ class SCOntology:
             self._get_bmi(obj),
             self._string(obj['consortium_name']),
             self._string(obj['provider_name']),
-            self._string(obj['provider_uuid']))
-        self._add_samples(donor_iri, None, obj['samples'])
+            self._string(obj['provider_uuid']),
+            publisher)
+        if 'samples' in obj:
+            self._add_samples(donor_iri, None, obj['samples'], publisher)
 
     def _add_donor_to_graph(self, donor_iri, description, comment, link,
                             age, biological_sex, bmi, consortium_name,
-                            provider_name, provider_uuid):
+                            provider_name, provider_uuid, publisher):
         self.graph.add((donor_iri, RDF.type, OWL.NamedIndividual))
         self.graph.add((donor_iri, RDF.type, CCF.donor))
         self.graph.add((donor_iri, CCF.description, description))
@@ -80,8 +90,9 @@ class SCOntology:
         self.graph.add((donor_iri, CCF.consortium_name, consortium_name))
         self.graph.add((donor_iri, CCF.tissue_provider_name, provider_name))
         self.graph.add((donor_iri, CCF.tissue_provider_uuid, provider_uuid))
+        self.graph.add((donor_iri, DCTERMS.publisher, publisher))
 
-    def _add_samples(self, donor_iri, tissue_block, any_samples):
+    def _add_samples(self, donor_iri, tissue_block, any_samples, publisher):
         for any_sample in any_samples:
             sample_type = any_sample['sample_type']
             if sample_type == "Tissue Block":
@@ -98,10 +109,17 @@ class SCOntology:
                     self._string(tissue_block['link']),
                     self._integer(tissue_block['section_count']),
                     self._integer(tissue_block['section_size']),
-                    self._string(tissue_block['section_units']))
-                self._add_samples(donor_iri, tissue_block,
-                                  tissue_block['sections'])
-                self._add_datasets(tissue_block, tissue_block['datasets'])
+                    self._string(tissue_block['section_units']),
+                    publisher)
+                if 'sections' in tissue_block:
+                    self._add_samples(donor_iri,
+                                      tissue_block,
+                                      tissue_block['sections'],
+                                      publisher)
+                if 'datasets' in tissue_block:
+                    self._add_datasets(tissue_block,
+                                       tissue_block['datasets'],
+                                       publisher)
             elif sample_type == "Tissue Section":
                 tissue_section = any_sample
                 if tissue_block is None:
@@ -115,16 +133,23 @@ class SCOntology:
                     self._string(tissue_section['label']),  # more like a comment
                     self._string(tissue_section['description']),
                     self._string(tissue_section['link']),
-                    self._integer(tissue_section['section_number']))
-                self._add_samples(donor_iri, tissue_section,
-                                  tissue_section['samples'])
-                self._add_datasets(tissue_section, tissue_section['datasets'])
+                    self._integer(tissue_section['section_number']),
+                    publisher)
+                if 'samples' in tissue_section:
+                    self._add_samples(donor_iri,
+                                      tissue_section,
+                                      tissue_section['samples'],
+                                      publisher)
+                if 'datasets' in tissue_section:
+                    self._add_datasets(tissue_section,
+                                       tissue_section['datasets'],
+                                       publisher)
 
     def _add_tissue_block_to_graph(self, tissue_block_iri,
                                    registration_location_iri,
                                    donor_iri, comment, description, link,
                                    section_count, section_size,
-                                   section_size_unit):
+                                   section_size_unit, publisher):
         self.graph.add((tissue_block_iri, RDF.type, OWL.NamedIndividual))
         self.graph.add((tissue_block_iri, RDF.type, CCF.tissue_block))
         self.graph.add((tissue_block_iri, CCF.has_registration_location,
@@ -138,11 +163,12 @@ class SCOntology:
         self.graph.add((tissue_block_iri, CCF.section_size, section_size))
         self.graph.add((tissue_block_iri, CCF.section_size_unit,
                         section_size_unit))
+        self.graph.add((tissue_block_iri, DCTERMS.publisher, publisher))
 
     def _add_tissue_section_to_graph(self, tissue_block_iri,
                                      tissue_section_iri, donor_iri,
                                      comment, description, link,
-                                     section_number):
+                                     section_number, publisher):
         self.graph.add((tissue_section_iri, RDF.type, OWL.NamedIndividual))
         self.graph.add((tissue_section_iri, RDF.type, CCF.tissue_section))
         self.graph.add((tissue_section_iri, CCF.part_of_tissue_block,
@@ -156,8 +182,9 @@ class SCOntology:
         self.graph.add((tissue_section_iri, CCF.url, link))
         self.graph.add((tissue_section_iri, CCF.section_number,
                         section_number))
+        self.graph.add((tissue_section_iri, DCTERMS.publisher, publisher))
 
-    def _add_datasets(self, any_sample, datasets):
+    def _add_datasets(self, any_sample, datasets, publisher):
         for dataset in datasets:
             self._add_dataset_to_graph(
                 self._uri(dataset['@id']),
@@ -166,10 +193,12 @@ class SCOntology:
                 self._string(dataset['description']),
                 self._string(dataset['link']),
                 self._string(dataset['technology']),
-                self._string(dataset['thumbnail']))
+                self._string(dataset['thumbnail']),
+                publisher)
 
     def _add_dataset_to_graph(self, dataset_iri, sample_iri, comment,
-                              description, link, technology, thumbnail):
+                              description, link, technology, thumbnail,
+                              publisher):
         self.graph.add((dataset_iri, RDF.type, OWL.NamedIndividual))
         self.graph.add((dataset_iri, RDF.type, CCF.dataset))
         self.graph.add((sample_iri, CCF.generates_dataset, dataset_iri))
@@ -178,6 +207,13 @@ class SCOntology:
         self.graph.add((dataset_iri, CCF.url, link))
         self.graph.add((dataset_iri, CCF.technology, technology))
         self.graph.add((dataset_iri, CCF.thumbnail, thumbnail))
+        self.graph.add((dataset_iri, DCTERMS.publisher, publisher))
+
+    def _get_publisher(self, obj):
+        try:
+            return self._string(obj['consortium_name'])
+        except KeyError:
+            return None
 
     def _get_age(self, obj):
         try:
